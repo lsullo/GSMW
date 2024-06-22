fc9c54a3f0e39c0db7da0911586e16ebb45774c6
lsullo <lsullo2004@gmail.com>
2024-06-22 18:38:21 -0500

    og resource trying with incog

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,18 +1,9 @@
-import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
+import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
 
-/*== STEP 1 ===============================================================
-The section below creates a Todo database table with a "content" field and
-a "isDone" field. The authorization rule below specifies that any authenticated
-user can "create", "read", "update", and "delete" their own "Todo" records.
-=========================================================================*/
 const schema = a.schema({
   Todo: a.model({
-      content: a.string(),
-      isDone: a.boolean(),
-      owner: a.string(), // Field to store the user's ID
-    }).authorization(rules => [
-      rules.owner()
-    ]),
+    content: a.string(),
+  }).authorization(allow => [allow.owner()]),
 });
 
 export type Schema = ClientSchema<typeof schema>;
@@ -20,6 +11,8 @@ export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: "userPool",
+    // This tells the data client in your app (generateClient())
+    // to sign API requests with the user authentication token.
+    defaultAuthorizationMode: 'userPool',
   },
 });
